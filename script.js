be9c2137ebdc61cb7be16b4e8f869e8642aefb3b
pythava <pythava1103@gmail.com>
2026-01-29 15:12:59 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// ===== 자동 슬라이드 JS =====
+const slides = document.querySelector('.slides');
+const totalSlides = slides.children.length;
+let index = 0;
+
+function showSlide() {
+    index++;
+    if(index >= totalSlides) index = 0;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+// 3초마다 슬라이드 자동 전환
+setInterval(showSlide, 3000);
